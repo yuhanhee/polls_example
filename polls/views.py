f3e4c2af3c86b27c814e2091592c91d567b104e4
yuhanhee <hanhee3089@hs.ac.kr>
2022-05-04 15:16:58 +0900

    Initial commit

---
+++ polls/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, get_object_or_404
+from polls.models import Question, Choice
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+
+def index(request):
+    latest_question_list=Question.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+
+    return render(request, 'polls/index.html', context)
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def vote(reqeust, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice =question.choice_set.get(pk=reqeust.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(reqeust, 'polls/detail.html', {'question':question, 'error_message':"You did'nt select a choice."})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
